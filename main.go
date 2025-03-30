42c28911f836e84d2c3e511ef24d8b6f44d4e3ae
Young, Gianni <cmancy@tutanota.com>
2025-03-30 12:56:19 -0500

    plan for outputting quotes

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
 	"os"
 	"strings"
@@ -13,6 +14,16 @@ import (
 const apiURL = "https://api.hardcover.app/v1/graphql"
 
 var authToken string
+
+type PrettyQuote struct {
+	Quote                string
+	BookTitle            string
+	BookSubTitle         string
+	BookAuthor           string
+	HardcoverUser        string
+	HardcoverFlair       string
+	HardcoverProfileLink string
+}
 
 type Response struct {
 	Me []struct {
@@ -103,6 +114,10 @@ func init() {
 
 }
 
+func printQuote(quote PrettyQuote) {
+
+}
+
 func main() {
 	client := graphql.NewClient(apiURL)
 	ctx := context.Background()
@@ -117,7 +132,8 @@ func main() {
 		quoted_book_ids = append(quoted_book_ids, quotedBook.Book.Book_id)
 	}
 
-	random_book := quotedBooks[rand.Intn(len(quoted_book_ids))].Book
+	random_book_index := rand.Intn(len(quoted_book_ids))
+	random_book := quotedBooks[random_book_index].Book
 
 	if len(user_info_response.Me) > 0 {
 		log.Info("", "Username", user_info_response.Me[0].Username)
@@ -132,4 +148,23 @@ func main() {
 	}
 
 	log.Infof("Finding random quote from %s", random_book.Book_title)
+	number_of_quotes := len(quotedBooks[random_book_index].Reading_journals)
+	quote := strings.TrimSpace(
+		quotedBooks[random_book_index].Reading_journals[rand.Intn(number_of_quotes)].Quote,
+	)
+	log.Info(quote)
+	exportedQuote := PrettyQuote{
+		Quote:          quote,
+		BookTitle:      random_book.Book_title,
+		BookSubTitle:   random_book.Book_subtitle,
+		BookAuthor:     random_book.Contributions[0].Author.Name,
+		HardcoverUser:  user_info_response.Me[0].Username,
+		HardcoverFlair: user_info_response.Me[0].Flair,
+		HardcoverProfileLink: fmt.Sprintf(
+			"https://hardcover.app/@%s",
+			user_info_response.Me[0].Username,
+		),
+	}
+
+	printQuote(exportedQuote)
 }
